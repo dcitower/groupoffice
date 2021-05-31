488b2c6df7e4a19853e797e444c4e631bfda80ba
Shubham Bhattacharya <shubhambhattacharya.02@gmail.com>
2021-05-31 09:46:44 +0530

    adding uninstall method

--- www/go/modules/community/filteraddressbook/Module.php
+++ www/go/modules/community/filteraddressbook/Module.php
@@ -13,5 +13,273 @@ class Module extends core\Module {
 	public function getAuthor() {
 		return "Intermesh BV <info@intermesh.nl>";
 	}
-							
+			
+	/* 
+	* Change core method codes after install 
+	*/
+	protected function afterInstall(\go\core\model\Module $model){
+
+		// Alter the core method of go\core\jmap\EntityController
+
+		uopz_add_function(\go\core\jmap\EntityController::class,'createEntitites', function () {
+			foreach ($create as $clientId => $properties) {
+
+				$entity = $this->create($properties);
+
+				if(!$this->canCreate($entity)) {
+					$result['notCreated'][$clientId] = new SetError("forbidden", go()->t("Permission denied"));
+					continue;
+				}
+
+				if ($entity->save()) {
+
+					// save address book
+					if(isset($properties['filter'])){
+						$addressBook = new \go\modules\community\addressbook\model\AddressBook();
+						$addressBook->name = '~ '.$properties['name'];
+						$addressBook->salutationTemplate = 'Dear [if {{contact.prefixes}}]{{contact.prefixes}}[else][if !{{contact.gender}}]Ms./Mr.[else][if {{contact.gender}}=="M"]Mr.[else]Ms.[/if][/if][/if][if {{contact.middleName}}] {{contact.middleName}}[/if] {{contact.lastName}}';
+						$addressBook->aclId = $properties['acl'][2];
+
+						$addressBook->save();
+
+
+						$arr['sort'] = [];
+						$subarr['property'] = 'name';
+						$subarr['isAscending'] = 1;
+						array_push($arr['sort'],$subarr);
+						$arr['calculateTotal'] = 1;
+						$arr['limit'] = 40; 
+
+						$opr = $properties['filter']['operator'];
+						$condition = $properties['filter']['conditions'];
+						$flArr['operator'] = $opr;
+						$flArr['conditions'] = $condition;
+						$properties['filter']['conditions'] = [];
+						array_push($properties['filter']['conditions'], $flArr);
+						$arr['filter'] = $properties['filter'];
+
+						$cont =  new \go\modules\community\addressbook\controller\Contact();
+
+						$fl = $cont->query($arr);
+
+
+
+						// Iterate through all IDs
+						foreach ($fl['ids'] as $key => $id) {
+
+							$customFilter = new \go\modules\community\filteraddressbook\model\ContactMap();
+							$customFilter->addressBookId = $addressBook->id;
+							$customFilter->contactId = $id;
+							$customFilter->save();
+						}
+
+					}
+
+					//refetch from server when mapping has a query object.
+					if($entity::getMapping()->getQuery() != null) {
+						$entity = $this->getEntity($entity->id());
+					}
+
+					$entityProps = new ArrayObject($entity->toArray());
+					$diff = $entityProps->diff($properties);
+					$diff['id'] = $entity->id();
+
+					$result['created'][$clientId] = empty($diff) ? null : $diff;
+				} else {				
+					$result['notCreated'][$clientId] = new SetError("invalidProperties");
+					$result['notCreated'][$clientId]->properties = array_keys($entity->getValidationErrors());
+					$result['notCreated'][$clientId]->validationErrors = $entity->getValidationErrors();
+				}
+			}
+		});
+		uopz_add_function(\go\core\jmap\EntityController::class,'defaultQuery',function(){
+				$p = $this->paramsQuery($params);
+				$idsQuery = $this->getQueryQuery($p);
+				
+				$idsQuery->fetchMode(PDO::FETCH_NUM);
+				$state = $this->getState();
+				
+				$ids = [];		
+		
+				try {
+					foreach ($idsQuery as $record) {
+						if (!isset($count)) {
+							$count = count($record);
+						}
+						$ids[] = $count ? $record[0] : implode('-', $record);
+					}
+		
+					// check if ID has data
+					if(count($ids) == 0){
+						// Get data for addressbook
+						if(isset($params['filter']['addressBookId'])){
+							$contactIds = (new Query)
+								->select('contactId')
+								->from('filteraddressbook_contact_map')
+								->where(['addressBookId' => $params['filter']['addressBookId']])
+								->all();
+							foreach ($contactIds as $key => $contactId) {	
+								$ids[] = $contactId['contactId'];
+							}
+						}
+					}
+		
+					if($p['calculateHasMore'] && count($ids) > $params['limit']) {
+						$hasMore = !!array_pop($ids);
+					}
+		
+					$response = [
+						'accountId' => $p['accountId'],
+						'state' => $state,
+						'ids' => $ids,
+						'notfound' => [],
+						'canCalculateUpdates' => false
+					];
+		
+					if(isset($hasMore)) {
+						$response['hasMore'] = $hasMore;
+					}
+		
+					if ($p['calculateTotal']) {
+		
+						if($idsQuery->getCalcFoundRows()) {
+							$response['total'] = $idsQuery->foundRows();
+						} else{
+							 $totalQuery = clone $idsQuery;
+		
+							 if(count($idsQuery->getGroupBy())) {
+								 $totalQuery->selectSingleValue("count(distinct " . $totalQuery->getTableAlias() . ".id)");
+							 } else{
+								 //count(*) can be used because we use a subquery in AclItemEntity::applyAclToQuery()
+								 $totalQuery->selectSingleValue("count(*)");
+							 }
+		
+							 $response['total'] = $totalQuery
+		
+															 ->orderBy([], false)
+															 ->groupBy([])
+															 ->limit(1)
+															 ->offset(0)
+															 ->single();
+						}
+					}
+				}catch(PDOException $e) {
+		
+					//Check if the PDOException is due to an invalid sort
+					//SQLSTATE[42S22]: Column not found: 1054 Unknown column 'customFields.A_checkbox' in 'order clause'
+					$msg = $e->getMessage();
+					if(strpos($msg, '42S22') !== false && strpos($msg, 'order clause') !== false) {
+						throw new UnsupportedSort();
+					} else{
+						throw $e;
+					}
+				}
+				
+				return $response;
+		});
+		return parent::afterInstall($model);
+	}
+
+	protected function beforeUninstall(\go\core\model\Module $model){
+		uopz_add_function(\go\core\jmap\EntityController::class,'createEntitites', function () {
+			foreach ($create as $clientId => $properties) {
+
+				$entity = $this->create($properties);
+
+				if(!$this->canCreate($entity)) {
+					$result['notCreated'][$clientId] = new SetError("forbidden", go()->t("Permission denied"));
+					continue;
+				}
+
+				if ($entity->save()) {
+
+					//refetch from server when mapping has a query object.
+					if($entity::getMapping()->getQuery() != null) {
+						$entity = $this->getEntity($entity->id());
+					}
+
+					$entityProps = new ArrayObject($entity->toArray());
+					$diff = $entityProps->diff($properties);
+					$diff['id'] = $entity->id();
+
+					$result['created'][$clientId] = empty($diff) ? null : $diff;
+				} else {				
+					$result['notCreated'][$clientId] = new SetError("invalidProperties");
+					$result['notCreated'][$clientId]->properties = array_keys($entity->getValidationErrors());
+					$result['notCreated'][$clientId]->validationErrors = $entity->getValidationErrors();
+				}
+			}
+		});
+		uopz_add_function(\go\core\jmap\EntityController::class,'defaultQuery',function(){
+			$p = $this->paramsQuery($params);
+			$idsQuery = $this->getQueryQuery($p);
+			
+			$idsQuery->fetchMode(PDO::FETCH_NUM);
+			$state = $this->getState();
+			
+			$ids = [];		
+	
+			try {
+				foreach ($idsQuery as $record) {
+					if (!isset($count)) {
+						$count = count($record);
+					}
+					$ids[] = $count ? $record[0] : implode('-', $record);
+				}
+	
+				if($p['calculateHasMore'] && count($ids) > $params['limit']) {
+					$hasMore = !!array_pop($ids);
+				}
+	
+				$response = [
+					'accountId' => $p['accountId'],
+					'state' => $state,
+					'ids' => $ids,
+					'notfound' => [],
+					'canCalculateUpdates' => false
+				];
+	
+				if(isset($hasMore)) {
+					$response['hasMore'] = $hasMore;
+				}
+	
+				if ($p['calculateTotal']) {
+	
+					if($idsQuery->getCalcFoundRows()) {
+						$response['total'] = $idsQuery->foundRows();
+					} else{
+						 $totalQuery = clone $idsQuery;
+	
+						 if(count($idsQuery->getGroupBy())) {
+							 $totalQuery->selectSingleValue("count(distinct " . $totalQuery->getTableAlias() . ".id)");
+						 } else{
+							 //count(*) can be used because we use a subquery in AclItemEntity::applyAclToQuery()
+							 $totalQuery->selectSingleValue("count(*)");
+						 }
+	
+						 $response['total'] = $totalQuery
+	
+														 ->orderBy([], false)
+														 ->groupBy([])
+														 ->limit(1)
+														 ->offset(0)
+														 ->single();
+					}
+				}
+			}catch(PDOException $e) {
+	
+				//Check if the PDOException is due to an invalid sort
+				//SQLSTATE[42S22]: Column not found: 1054 Unknown column 'customFields.A_checkbox' in 'order clause'
+				$msg = $e->getMessage();
+				if(strpos($msg, '42S22') !== false && strpos($msg, 'order clause') !== false) {
+					throw new UnsupportedSort();
+				} else{
+					throw $e;
+				}
+			}
+			
+			return $response;
+	});
+		return parent::afterInstall($model);	
+	}
 }
