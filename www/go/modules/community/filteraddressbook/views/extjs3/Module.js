c69418e1de95428c4079ed9e1f7bc67c250d3b6a
Shubham Bhattacharya <shubhambhattacharya.02@gmail.com>
2021-06-10 22:50:56 +0530

    Adding all codes for filter address book

---
+++ www/go/modules/community/filteraddressbook/views/extjs3/Module.js
@@ -0,0 +1,6 @@
+go.Modules.register("community", "filteraddressbook", {
+	mainPanel: "go.modules.community.filteraddressbook.MainPanel",
+	title: t("Filter Address Book"),
+	entities: [],
+	initModule: function () {}
+});
